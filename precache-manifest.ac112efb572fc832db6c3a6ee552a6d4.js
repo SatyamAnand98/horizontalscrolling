46a59f7b12092ccc23c01419132e934a23c19978
SatyamAnand98 <thesatemail@gmail.com>
2020-08-01 17:47:47 +0530

    Updates

---
+++ precache-manifest.ac112efb572fc832db6c3a6ee552a6d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d81f38e7ca674c733931376b572bc9",
+    "url": "/horizontalscrolling/index.html"
+  },
+  {
+    "revision": "fb121291e0a0f7e29356",
+    "url": "/horizontalscrolling/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fe6204b025b9fc9e36b",
+    "url": "/horizontalscrolling/static/css/main.f34bb469.chunk.css"
+  },
+  {
+    "revision": "fb121291e0a0f7e29356",
+    "url": "/horizontalscrolling/static/js/2.65408416.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/horizontalscrolling/static/js/2.65408416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe6204b025b9fc9e36b",
+    "url": "/horizontalscrolling/static/js/main.35844f19.chunk.js"
+  },
+  {
+    "revision": "a2a7fc6a67b9420d252b",
+    "url": "/horizontalscrolling/static/js/runtime-main.a1a33996.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/horizontalscrolling/static/media/logo.5d5d9eef.svg"
+  }
+]);
